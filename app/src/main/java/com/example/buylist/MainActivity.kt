457f263e498dab6fc4617f5ab9bc249e8088ebd7
Dmirtykabachok <d.kovkov189@gmail.com>
2021-07-24 14:37:43 +0300

    2407

--- app/src/main/java/com/example/buylist/MainActivity.kt
+++ app/src/main/java/com/example/buylist/MainActivity.kt
@@ -1,41 +1,61 @@
 package com.example.buylist
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Delete
 import kotlinx.coroutines.*
+import java.lang.ref.WeakReference
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var saveButton: Button
     private lateinit var editText: EditText
-    private lateinit var textView: TextView
     private lateinit var countText: EditText
     private lateinit var loadButton: Button
     private lateinit var db: AppDatabase
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: Adapter
+    lateinit var adapterList: ArrayList<Model>
+    private var list: ArrayList<Item> = ArrayList()
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         editText = findViewById(R.id.edit_name)
         countText = findViewById(R.id.edit_count)
-        textView = findViewById(R.id.list)
         saveButton = findViewById(R.id.Save_btn)
         loadButton = findViewById(R.id.Load_btn)
+        recyclerView = findViewById(R.id.recycler)
+        adapterList = Model.convert(list)
+        adapter = Adapter { item-> deleteItem(item as Item) }
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
         db = (applicationContext as DatabaseCreateProv).getDatabase()
 
-        loadButton.setOnClickListener{
-            Log.d("DATABASE_ACTIVE","LOAD")
+
+        loadButton.setOnClickListener {
+            Log.d("DATABASE_ACTIVE", "LOAD")
             CoroutineScope(Dispatchers.IO).launch {
-                val list = db.itemDao().getAll()
-                withContext(Dispatchers.Main){
-                    textView.text = list.toString()
+                val dblist = db.itemDao().getAll()
+                list.clear()
+                dblist.forEach {
+                    list.add(it)
                 }
             }
+            adapter.setData(list)
         }
 
         saveButton.setOnClickListener {
@@ -52,4 +72,19 @@ class MainActivity : AppCompatActivity() {
         db.itemDao().insert(item)
         Log.d("DATABASE_ACTIVE", "SAVED")
     }
+
+    private fun deleteItem(item: Item) {
+        CoroutineScope(Dispatchers.IO).launch {
+            Log.d("DATABASE_ACTIVE", "DELETED")
+            db.itemDao().delete(item)
+            val dblist = db.itemDao().getAll()
+            list.clear()
+            dblist.forEach {
+                list.add(it)
+            }
+        }
+        adapter.setData(list)
+        }
+
+
 }
